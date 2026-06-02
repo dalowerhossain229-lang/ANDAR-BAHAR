edc703feebb43b2b856379cca89f1241d957b7ec
dalowerhossain229-lang <dalowerhossain229@gmail.com>
2026-06-03 02:15:34 +0600

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
 });
 
 // 🎰 [উইনগো কালার ট্রেড ওরিজিনাল ডোমেইন সিঙ্ক ভাই ভাই]
-const MAIN_SITE_URL = "https://onrender.com"; 
+const MAIN_SITE_URL = "https://betlover247.onrender.com"; 
 
 // 🃏 ওরিজিনাল ইন্টারন্যাশনাল কার্ড সুটস এবং র‍্যাঙ্ক পুল
 const cardSuitsPool = ["HEARTS", "DIAMONDS", "CLUBS", "SPADES"];
